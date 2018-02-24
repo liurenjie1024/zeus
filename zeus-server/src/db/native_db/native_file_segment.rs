07cfbebc061998bdacb2ce1dda84219da8737fbb
liurenjie1024 <liurenjie2008@gmail.com>
2018-02-24 16:06:36 +0800

    part

--- zeus-server/src/db/native_db/native_file_segment.rs
+++ zeus-server/src/db/native_db/native_file_segment.rs
@@ -1,11 +1,22 @@
+use std::path::Path;
+
 use util::error::Result;
+use util::error::Error;
+use std::io::ErrorKind;
 
 pub struct NativeFileSegment {
     path: String
 }
 
 impl NativeFileSegment {
-    pub fn validate() -> Result<()> {
+    pub fn validate(&self) -> Result<()> {
+        let path = Path::new(self.path);
 
+        if path.exists() && path.is_file() {
+            Ok(())
+        } else {
+            let msg = format!("{} not found", self.path);
+            std::io::Error::new(ErrorKind::NotFound, msg)?
+        }
     }
 }
