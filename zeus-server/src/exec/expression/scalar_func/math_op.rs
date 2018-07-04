8c09b886b46181e3c7888f212e2389f59ffa615f
liurenjie1024 <liurenjie2008@gmail.com>
2018-07-04 10:57:48 +0800

    math operator bug fix

--- zeus-server/src/exec/expression/scalar_func/math_op.rs
+++ zeus-server/src/exec/expression/scalar_func/math_op.rs
@@ -10,7 +10,8 @@ use util::errors::*;
 
 pub struct MathOperator {
   result_type: ColumnType,
-  math_op: fn(&Datum, &Datum) -> Result<Datum>
+  math_op: fn(&Datum, &Datum) -> Result<Datum>,
+  eval_fn: fn(&Datum) -> Result<Datum>
 }
 
 impl ScalarFunc for MathOperator {
@@ -30,31 +31,131 @@ impl ScalarFunc for MathOperator {
 }
 
 impl MathOperator {
-  pub fn add(result_type: ColumnType) -> MathOperator {
+  pub fn add_i32() -> MathOperator {
     MathOperator {
-      result_type,
-      math_op: Datum::add
+      result_type: ColumnType::INT32,
+      math_op: Datum::add,
+      eval_fn: |r| r.to_i32().map(|x| x.into())
     }
   }
 
-  pub fn minus(result_type: ColumnType) -> MathOperator {
+  pub fn add_i64() -> MathOperator {
     MathOperator {
-      result_type,
-      math_op: Datum::minus
+      result_type: ColumnType::INT64,
+      math_op: Datum::add,
+      eval_fn: |r| r.to_i64().map(|x| x.into())
     }
   }
 
-  pub fn multiply(result_type: ColumnType) -> MathOperator {
+  pub fn add_f4() -> MathOperator {
     MathOperator {
-      result_type,
-      math_op: Datum::multiply
+      result_type: ColumnType::FLOAT4,
+      math_op: Datum::add,
+      eval_fn: |r| r.to_f32().map(|x| x.into())
     }
   }
 
-  pub fn divide(result_type: ColumnType) -> MathOperator {
+  pub fn add_f8() -> MathOperator {
     MathOperator {
-      result_type,
-      math_op: Datum::divide
+      result_type: ColumnType::FLOAT8,
+      math_op: Datum::add,
+      eval_fn: |r| r.to_f64().map(|x| x.into())
+    }
+  }
+
+  pub fn minus_i32() -> MathOperator {
+    MathOperator {
+      result_type: ColumnType::INT32,
+      math_op: Datum::minus,
+      eval_fn: |r| r.to_i32().map(|x| x.into())
+    }
+  }
+
+  pub fn minus_i64() -> MathOperator {
+    MathOperator {
+      result_type: ColumnType::INT64,
+      math_op: Datum::minus,
+      eval_fn: |r| r.to_i64().map(|x| x.into())
+    }
+  }
+
+  pub fn minus_f4() -> MathOperator {
+    MathOperator {
+      result_type: ColumnType::FLOAT4,
+      math_op: Datum::minus,
+      eval_fn: |r| r.to_f32().map(|x| x.into())
+    }
+  }
+
+  pub fn minus_f8() -> MathOperator {
+    MathOperator {
+      result_type: ColumnType::FLOAT8,
+      math_op: Datum::minus,
+      eval_fn: |r| r.to_f64().map(|x| x.into())
+    }
+  }
+
+  pub fn multiply_i32() -> MathOperator {
+    MathOperator {
+      result_type: ColumnType::INT32,
+      math_op: Datum::multiply,
+      eval_fn: |r| r.to_i32().map(|x| x.into())
+    }
+  }
+
+  pub fn multiply_i64() -> MathOperator {
+    MathOperator {
+      result_type: ColumnType::INT64,
+      math_op: Datum::multiply,
+      eval_fn: |r| r.to_i64().map(|x| x.into())
+    }
+  }
+
+  pub fn multiply_f4() -> MathOperator {
+    MathOperator {
+      result_type: ColumnType::FLOAT4,
+      math_op: Datum::multiply,
+      eval_fn: |r| r.to_f32().map(|x| x.into())
+    }
+  }
+
+  pub fn multiply_f8() -> MathOperator {
+    MathOperator {
+      result_type: ColumnType::FLOAT8,
+      math_op: Datum::multiply,
+      eval_fn: |r| r.to_f64().map(|x| x.into())
+    }
+  }
+
+  pub fn divide_i32() -> MathOperator {
+    MathOperator {
+      result_type: ColumnType::INT32,
+      math_op: Datum::divide,
+      eval_fn: |r| r.to_i32().map(|x| x.into())
+    }
+  }
+
+  pub fn divide_i64() -> MathOperator {
+    MathOperator {
+      result_type: ColumnType::INT64,
+      math_op: Datum::divide,
+      eval_fn: |r| r.to_i64().map(|x| x.into())
+    }
+  }
+
+  pub fn divide_f4() -> MathOperator {
+    MathOperator {
+      result_type: ColumnType::FLOAT4,
+      math_op: Datum::divide,
+      eval_fn: |r| r.to_f32().map(|x| x.into())
+    }
+  }
+
+  pub fn divide_f8() -> MathOperator {
+    MathOperator {
+      result_type: ColumnType::FLOAT8,
+      math_op: Datum::divide,
+      eval_fn: |r| r.to_f64().map(|x| x.into())
     }
   }
 }
