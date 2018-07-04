9bcec1f195095455e8a72c19f7cf86a965bef80a
liurenjie1024 <liurenjie2008@gmail.com>
2018-07-04 11:02:05 +0800

    fix bug

--- zeus-server/src/exec/expression/scalar_func/math_op.rs
+++ zeus-server/src/exec/expression/scalar_func/math_op.rs
@@ -19,7 +19,9 @@ impl ScalarFunc for MathOperator {
     let vec_data = input.columns_slice()[0].iter()
       .zip(input.columns_slice()[1].iter())
       .try_fold(Vec::new(), |mut ret, pair| -> Result<Vec<Datum>> {
-        ret.push((self.math_op)(&pair.0, &pair.1)?);
+        let left = eval_fn(pair.0)?;
+        let right = eval_fn(pair.1)?;
+        ret.push((self.math_op)(&left, &right)?);
         Ok(ret)
       })?;
 
