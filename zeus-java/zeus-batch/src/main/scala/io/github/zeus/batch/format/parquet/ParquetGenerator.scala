f6b856f5778d4cd90f353ffd97e03cbd852432de
liurenjie1024 <liurenjie2008@gmail.com>
2018-07-05 11:14:31 +0800

    fix bug

--- zeus-java/zeus-batch/src/main/scala/io/github/zeus/batch/format/parquet/ParquetGenerator.scala
+++ zeus-java/zeus-batch/src/main/scala/io/github/zeus/batch/format/parquet/ParquetGenerator.scala
@@ -1,6 +1,6 @@
 package io.github.zeus.batch.format.parquet
 
-import com.mediav.data.log.unitedlog.UnitedEvent
+import com.mediav.realtime.log.RealtimeLog
 import io.github.zeus.tool.thrift.spark.ThriftDataFrameBuilder
 import org.apache.spark.sql.SparkSession
 
@@ -10,14 +10,14 @@ object ParquetGenerator {
       .appName("zeus-parquet-generator")
       .getOrCreate()
 
-    val df = new ThriftDataFrameBuilder[UnitedEvent, UnitedEvent._Fields]("/mvad/rawlog/dsp-charge/2018-07-03/*/dsp.charge.6.click/*")
+    val df = new ThriftDataFrameBuilder[RealtimeLog, RealtimeLog._Fields]("/mvad/rawlog/dsp-charge/2018-07-03/*/dsp.charge.6.click/*")
       .build(spark)
       .limit(1000)
 
     println(s"Dataframe size is ${df.count()}")
 
-//      .coalesce(1)
-//      .write.parquet("rt-parquet")
+    df.coalesce(1)
+      .write.parquet("rt-parquet")
 
     spark.close()
   }
