cf49b923799d0ad24c49af7016ddf4ed942e5808
liurenjie1024 <liurenjie2008@gmail.com>
2018-07-05 10:56:13 +0800

    uncompressed generator

--- zeus-java/zeus-batch/run_spark.sh
+++ zeus-java/zeus-batch/run_spark.sh
@@ -4,7 +4,7 @@ export SPARK_HOME=/opt/spark-2.1.0-cdh5.8.0
 echo "Begin to run spark"
 $SPARK_HOME/bin/spark-submit \
   --master yarn-client \
-  --class io.github.zeus.batch.format.parquet.ParquetReader \
+  --class io.github.zeus.batch.format.parquet.ParquetGenerator \
   --conf spark.executor.memory=12G \
   --conf spark.task.maxFailures=1000 \
   --conf spark.yarn.max.executor.failures=2000 \
@@ -12,6 +12,7 @@ $SPARK_HOME/bin/spark-submit \
   --conf spark.dynamicAllocation.enable=false \
   --conf spark.executor.instances=200 \
   --conf spark.driver.memory=60G \
+  --conf spark.sql.parquet.compression.codec=uncompressed \
   --queue adhoc \
   --driver-java-options "-XX:+UseG1GC -XX:+PrintGC -XX:+PrintGCDetails -Xloggc:spark.driver.gc.log" \
   --driver-memory 16G \
